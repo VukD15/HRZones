5c54a8de93c80ae1e54c3b4024571bde9638d329
VukGit <woolfpiroman@gmail.com>
2025-02-20 10:28:38 +0200

    fix prop-drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './index.css'
 import ButtonSection from "./components/ButtonSection.tsx";
 import HRInputArea from "./components/HRInputArea.tsx";
-import ZoneSection from "./components/ZoneSection.tsx";
 import {useState} from "react";
 import {HRZone} from "./logic/HRZone.ts";
 import MethodButton from "./components/MethodButton.tsx";
+import data from "./assets/data.json";
+import ZoneComponent from "./components/ZoneComponent.tsx";
+import ZoneRange from "./components/ZoneRange.tsx";
+import ZoneCalcResults from "./components/ZoneCalcResults.tsx";
+
+type ZoneObject = {
+    id: string,
+    zoneStyleCode: string,
+    minPercent: number,
+    maxPercent: number,
+    zoneDescription: string
+}
+
+const zoneData: ZoneObject[] = data;
 
 function App() {
     const [calcMethod, setCalcMethod] = useState("MHR");
@@ -27,7 +40,22 @@ function App() {
                 <MethodButton label={"LTHR"} isActive={calcMethod === "LTHR"} onCLickAction={handleCalcMeth}/>
             </ButtonSection>
             <HRInputArea selectedMethod={calcMethod} handleZoneCalculation={handleHrZones}/>
-            <ZoneSection hrCalcResults={hrZones}/>
+            <section className="flex-col bg-neutral-900 px-0.5 py-0.5 max-h-max">
+                {zoneData.map(function (zone: ZoneObject) {
+                    return (
+                        <ZoneComponent zoneStyleCode={zone.zoneStyleCode}>
+                            <ZoneRange zoneName={zone.id} minPercent={zone.minPercent} maxPercent={zone.maxPercent}/>
+                            <ZoneCalcResults zoneMin={hrZones[parseInt(zone.id) - 1]?.zoneMin}
+                                             zoneMax={hrZones[parseInt(zone.id) - 1]?.zoneMax}
+                            />
+                            <span
+                                className="text-gray-100 align-middle border-l-black border-solid border-l-10 p-2 max-w-3xl">
+                                 <p>{zone.zoneDescription}</p>
+                             </span>
+                        </ZoneComponent>)
+                })
+                }
+            </section>
         </>
     )
 }
--- src/components/ZoneComponent.tsx
+++ src/components/ZoneComponent.tsx
@@ -1,34 +1,14 @@
-import ZoneRange from "./ZoneRange.tsx";
-import ZoneCalcResults from "./ZoneCalcResults.tsx";
-import {HRZone} from "../logic/HRZone.ts";
+import React from "react";
 
 type ZoneComponentProps = {
-    zoneName: string;
     zoneStyleCode: string;
-    minPercent?: number;
-    maxPercent?: number;
-    zoneDescription: string;
-    zoneCalcResults?: HRZone;
+    children: React.ReactNode;
 }
 
-export default function ZoneComponent({
-                                          zoneName,
-                                          zoneStyleCode,
-                                          minPercent,
-                                          maxPercent,
-                                          zoneDescription,
-                                          zoneCalcResults
-                                      }: ZoneComponentProps) {
-
+export default function ZoneComponent({children, zoneStyleCode}: ZoneComponentProps) {
     return (
         <section className={`flex grow m-2 min-h-15 place-content-around ${zoneStyleCode}`}>
-            <>
-                <ZoneRange zoneName={zoneName} minPercent={minPercent} maxPercent={maxPercent}/>
-                <ZoneCalcResults zoneMin={zoneCalcResults?.zoneMin} zoneMax={zoneCalcResults?.zoneMax}/>
-                <span className="text-gray-100 align-middle border-l-black border-solid border-l-10 p-2 max-w-3xl">
-                  <span>{zoneDescription}</span>
-            </span>
-            </>
+            {children}
         </section>
     )
 }
